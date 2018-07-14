18c7df5535dceec0c915c9fa2e24c29104c6d1ae
youngZ <792163000@qq.com>
2018-07-15 00:33:19 +0800

    🎈Faster

--- 14_longest_common_prefix.ts
+++ 14_longest_common_prefix.ts
@@ -4,13 +4,19 @@ const longestCommonPrefix = (strs: string[]): string => {
 
   for (let i = 1, l1 = strs.length; i < l1; i++) {
     const item = strs[i];
-    for (let j = 0, l2 = item.length; j < l2; j++) {
+    for (let j = 0, l2 = str.length; j < l2; j++) {
       if (str[j] === item[j]) {
         t += str[j];
       } else {
         break;
       }
     }
+
+    // 如果没值，就不需要继续往下走了
+    if (!str) {
+      return '';
+    }
+
     str = t;
     t = '';
   }
@@ -18,4 +24,4 @@ const longestCommonPrefix = (strs: string[]): string => {
   return str;
 };
 
-// Emmmmmm, 性能超过30%。。。。有毛病，又提交了一次，又超过了73%。。。
+// Emmmmmm, 性能超过30%。。。。有毛病，又提交了一次，然后超过了73%。。。
--- build/14_longest_common_prefix.js
+++ build/14_longest_common_prefix.js
@@ -4,7 +4,7 @@ const longestCommonPrefix = (strs) => {
     let t = '';
     for (let i = 1, l1 = strs.length; i < l1; i++) {
         const item = strs[i];
-        for (let j = 0, l2 = item.length; j < l2; j++) {
+        for (let j = 0, l2 = str.length; j < l2; j++) {
             if (str[j] === item[j]) {
                 t += str[j];
             }
@@ -12,9 +12,13 @@ const longestCommonPrefix = (strs) => {
                 break;
             }
         }
+        // 如果没值，就不需要继续往下走了
+        if (!str) {
+            return '';
+        }
         str = t;
         t = '';
     }
     return str;
 };
-// Emmmmmm, 性能超过30%。。。。有毛病，又提交了一次，又超过了73%。。。
+// Emmmmmm, 性能超过30%。。。。有毛病，又提交了一次，然后超过了73%。。。
