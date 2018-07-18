62789f26f9e6784df5664fb1574a1786c8812e8f
yz <792163000@qq.com>
2018-07-18 16:21:59 +0800

    🎈threeSum🙃

---
+++ 15_3_sum.ts
@@ -0,0 +1,42 @@
+const threeSum = function(nums: number[]): any[] {
+  if (nums.length < 3) {
+    return [];
+  };
+  nums = nums.sort((a, b) => a - b);
+  if (nums[0] > 0 || nums[nums.length - 1] < 0) {
+    return [];
+  }
+  const result = [];
+
+  for (let i = 0, len = nums.length; i < len - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let left = i + 1;
+    let right = nums.length - 1;
+    // 这尼玛，又忘了有这个骚操作
+    let target = -nums[i];
+    
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (nums[left] + nums[right] === target) {
+        result.push([-target, nums[left], nums[right]]);
+        left++;
+        right--;
+
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      } else if (sum > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+
+  return result;
+};
